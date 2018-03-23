1978cc6c1e01d1507a6678d567cec190f527e9f2
JackXieFeifei <960172037@qq.com>
2018-03-23 18:27:46 +0800

    微调

--- WeChatDev/FirstLittleGame/pages/index/index.js
+++ WeChatDev/FirstLittleGame/pages/index/index.js
@@ -29,12 +29,7 @@ Page({
     animation: 0,
     animationIndex: -1
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+ 
   onLoad: function () {
     console.log("onLoad: function");
     if (app.globalData.userInfo) {
